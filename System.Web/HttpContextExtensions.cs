8cb6f676098c4c1d1f80966b292bba9c37f92dcc
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2018-12-17 16:34:18 +0000

    changes project type

--- System.Web/HttpContextExtensions.cs
+++ System.Web/HttpContextExtensions.cs
@@ -20,6 +20,33 @@ namespace System.Web
         /// <param name="func">The function to run</param>
         /// <param name="preparing">A method to run while preparing to run the async function</param>
         /// <param name="running">A method to run while running the async function</param>
+        public static void Run(this HttpContextBase context, Func<Task> func, Action<AsyncCallContext> preparing = null, Action<AsyncCallContext> running = null)
+        {
+            HttpContext.Current.Run(func, preparing, running);
+        }
+
+        /// <summary>
+        /// Run an async function ensuring that HttpContext won't be null on the way
+        /// </summary>
+        /// <typeparam name="T">The type that the function returns</typeparam>
+        /// <param name="context">The current HttpContext</param>
+        /// <param name="func">The function to run</param>
+        /// <param name="preparing">A method to run while preparing to run the async function</param>
+        /// <param name="running">A method to run while running the async function</param>
+        /// <returns>The response from the async function</returns>
+        public static T Run<T>(this HttpContextBase context, Func<Task<T>> func, Action<AsyncCallContext> preparing = null, Action<AsyncCallContext> running = null)
+        {
+            return HttpContext.Current.Run<T>(func, preparing, running);
+        }
+
+
+        /// <summary>
+        /// Run an async function ensuring that HttpContext won't be null on the way
+        /// </summary>
+        /// <param name="context">The current HttpContext</param>
+        /// <param name="func">The function to run</param>
+        /// <param name="preparing">A method to run while preparing to run the async function</param>
+        /// <param name="running">A method to run while running the async function</param>
         public static void Run(this HttpContext context, Func<Task> func, Action<AsyncCallContext> preparing = null, Action<AsyncCallContext> running = null)
         {
             context.Run<object>(async () =>
